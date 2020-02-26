6e4c0b3d0a2f8d1e3479d3fcf9539e860d98fc86
Yevhen_Lazhyntsev <lazhynce@gmail.com>
2020-02-26 20:56:49 +0200

    First version

---
+++ src/main/java/main.java
@@ -0,0 +1,49 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class main {
+    private static String pageLink = "https://rozetka.com.ua/produkty/c4624997/page=PageIndex;price=2-MaxPrice;sort=novelty/";
+    private static String maxPageNumber;
+    private static int maxPrise=10000;
+    private static int initialValue=maxPrise;
+    private static HashMap<String,Integer> result;
+    public static void main(String[] args) throws IOException {
+        List<Element> elementList;
+        result=new HashMap<>();
+        while (maxPrise >= 2) {
+            elementList = getRandomPage(maxPrise);
+            getRandomProduct(elementList);
+        }
+        System.out.println("-".repeat(135));
+        result.forEach((name,price) -> System.out.printf("%-130s|%s\n", name, price));
+        System.out.println("-".repeat(135));
+    }
+
+    public static List getRandomPage(int maxPrice) throws IOException {
+        maxPageNumber = String.valueOf((int) (Math.random() * 34));
+        Document doc;
+             doc = Jsoup.connect(pageLink.replace("MaxPrice", String.valueOf(maxPrice)).replace("PageIndex", maxPageNumber)).get();
+        List<Element> elementList = doc.body().getElementsByClass("catalog-grid__cell catalog-grid__cell_type_slim");
+        return elementList;
+    }
+
+    static public void getRandomProduct(List<Element> productsList){
+        System.out.println("Choosing Carefully"+".".repeat(100*maxPrise/initialValue)+" " + maxPrise);
+        int index = (int) (Math.random() * (productsList.size()));
+        String productName = productsList.get(index).getElementsByClass("goods-tile__title").text().replaceAll("\\([^)]*\\)","");
+        int productPrice = Integer.valueOf(productsList.get(index).getElementsByClass("goods-tile__price-value").text().replaceAll(" ",""));
+        maxPrise-=productPrice;
+        result.put(productName,productPrice);
+    }
+
+}
+
